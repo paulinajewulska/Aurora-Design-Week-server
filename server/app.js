29032ddd827b2ea723e05a9a35ee7caf73868129
paulinajewulska <paulina.jewulska@gmail.com>
2020-07-15 10:37:20 +0200

    Update port

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connectionString = process.env.MONGODB_URI;
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
